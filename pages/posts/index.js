af60a33fa43f107fe00f452a0a942c2b33a172e5
Alex Borey <lexnex@gmail.com>
2023-08-22 09:10:58 +0200

    fix paths

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
-import Layout, { siteTitle } from "../components/layout"
-import utilStyles from "../styles/utils.module.css"
+import Layout, { siteTitle } from "../../components/layout"
+import utilStyles from "../../styles/utils.module.css"
 
-import { getSortedPostsData } from "../lib/posts"
+import { getSortedPostsData } from "../../lib/posts"
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
